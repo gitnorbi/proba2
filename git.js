43570198a597d15666bf9dbfd167f6917c3665df
norbigith <dulnorbert@gmail.com>
2021-11-10 18:43:59 +0100

    prob1

---
+++ git.js
@@ -0,0 +1,22 @@
+/*
+git saves my changes in a special data base called repository
+-Repository is a special data based used by git to save my versions of the code im create
+
+THERARE 2 kind of version control system: centralized and distributed
+with centralized, if the server goes offline we cannot work with it!!
+distributed version control systems are better, and git is one of it like this system!
+
+Git is free, open source, distributed so its very popular!!
+
+i can use Git in:
+Comand line 
+and with code editors and Idess as well
+There are also a graphical user interfaces to use git with shot: GIU clients for Git, 2 most populars are:
+                                                                    mosh like:        GitKraken Git Gui
+                                                                                    SourceTree Gui
+
+For comand line: i can use pl the git Bash
+using comand line has a few benefits, such as u can still use git when u can not accses a GUI os something
+
+                                                                
+*/
